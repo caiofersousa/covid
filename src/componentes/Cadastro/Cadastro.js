35277945161465cca6ee9c1644d36306c54d5661
caiofersousa <caiofersousa@hotmail.com>
2023-09-21 20:09:26 -0300

    first commit

---
+++ src/componentes/Cadastro/Cadastro.js
@@ -0,0 +1,11 @@
+import './Cadastro.css';
+function Cadastro() {
+    return (
+        <div>
+            <h1>Cadastro</h1>
+            <input type="text" placeholder="nome"/>
+        </div>
+          );
+}
+
+export default Cadastro;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './componentes/App/App';
 import reportWebVitals from './reportWebVitals';
+import Cadastro from './componentes/Cadastro/Cadastro';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Cadastro />
     <App />
   </React.StrictMode>
 );
